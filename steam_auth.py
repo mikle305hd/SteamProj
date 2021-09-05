027422ba3670b41c794fd4ac14dad23c70f5c5d3
mikle305 <54461515+mikle305hd@users.noreply.github.com>
2021-09-05 21:14:31 +0300

    добавил SteamAuth в steam_auth

--- steam_auth.py
+++ steam_auth.py
@@ -1,3 +1,7 @@
+from datetime import datetime
+import requests
+
+
 class SteamData:
     def __init__(self, login, password):
         if (type(login) and type(password) == str):
@@ -25,3 +29,27 @@ class SteamData:
     @property
     def password(self):
         return self.__password
+
+class SteamAuth:
+    def __init__(self, login, password, steam_guard):
+        if ((type(login) == str and type(password) == str and type(steam_guard) == str) and len(str(steam_guard)) == 5):
+            self.__login = login
+            self.__password = password
+            self.__guard = str(steam_guard).upper()
+        else:
+            raise Exception("Не валидные данные для входа в аккаунт")
+
+    def __get_timestamp(self):
+        return int(datetime.now().timestamp() * 1000)
+
+    def do(self):
+        self.__url = 'https://steamcommunity.com/'
+        headers = {
+
+        }
+        with requests.session() as session:
+            url = self.__url + 'login/getrsakey/'
+            data = f'donotcache={self.__get_timestamp()}&username={self.__login}'
+            response = session.post(url, data, headers=headers)
+            print(response.status_code)
+            print(response)
