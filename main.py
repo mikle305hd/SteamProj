830fda9cd4d46c46b4987190fc908410d7eece8b
mikle305hd <mikle3305@gmail.com>
2021-09-12 15:56:04 +0300

    подкрутил json, UI и доделал авторизацию в стиме

--- main.py
+++ main.py
@@ -1,61 +1,74 @@
 import steam_auth
-import requests
+import json
 
 
 def main():
-    try:
-        auth = steam_auth.SteamAuth('mikle305hd', '213', '12345')
-    except Exception as e:
-        print(e)
-
-    auth.do()
-
+    user_data = None
     while True:
-        print('Введите комманду: ')
+        print('Введите комманду:', end= ' ')
         command = input().lower()
 
         if command == 'enter_userdata':
-            print('Введите имя пользователя')
+            print('Введите имя пользователя:', end=' ')
             login = input()
-            print('Введите пароль')
+            print('Введите пароль:', end=' ')
             password = input()
             try:
-                my_auth_data = steam_auth.SteamData(login, password)
+                user_data = steam_auth.SteamData(login, password)
             except Exception as e:
                 print(e)
                 continue
             login, password = '', ''
-            print('Если хотите сохранить данные в файл введите команду save')
+            print('Если хотите сохранить данные в файл введите команду save:', end=' ')
             if input().lower() == 'save':
                 try:
-                    with open('settings.txt', 'w') as file:
-                        file.write('username:' + my_auth_data.login + '\npassword:' + my_auth_data.password)
-                    del my_auth_data
+                    with open('auth_data.json', 'w') as file:
+                        data = {"username": user_data.login, "password": user_data.password}
+                        json.dump(data, file,indent=4)
+                        del data
+                    del user_data
                 except Exception as e:
                     print(e)
 
-        elif command == 'auth':
+        elif command == 'clear_userdata':
             try:
-                with open('settings.txt', 'r', encoding='utf8') as file:
-                    file_strings = file.readlines()
+                with open('auth_data.json', 'w') as file:
+                    json.dump({}, file)
             except Exception as e:
                 print(e)
-            for string in file_strings:
-                if string.find('username'):
-                    login = string.replace('login:', '')
-                if string.find('password'):
-                    password = string.replace('password', '')
+
+        elif command == 'auth_steam':
+            try:
+                with open('auth_data.json', 'r') as file:
+                    data = json.load(file)
+                    if data == {}:
+                        if user_data == None:
+                            print('Введите имя пользователя:', end= ' ')
+                            login = input()
+                            print('Введите пароль:', end=' ')
+                            password = input()
+                            user_data = steam_auth.SteamData(login, password)
+                    else:
+                        user_data = steam_auth.SteamData(data['username'], data['password'])
+
+                    print('Введите код Steam Guard:', end=' ')
+                    user_data.guard = input()
+
+                    client = steam_auth.SteamClient(user_data.login, user_data.password, user_data.guard)
+                    client.steam_auth()
+            except Exception as e:
+                print(e)
 
         elif command == 'exit':
+            print('Программа закрыта')
             return 0
 
         elif command == 'help':
             print('Список доступных комманд:'
-                  '\nenter_userdata  -  ввести/сохранить данные (имя пользователя и пароль) от аккаунта в файл;'
+                  '\nenter_userdata  -  ввести/сохранить данные (имя пользователя и пароль) аккаунта в файл;'
                   '\nclear_userdata  -  удалить данные аккаунта из файла'
-                  '\nauth  -  авторизоваться в стиме (сохранение данных в файл необязательно);'
+                  '\nauth_steam  -  авторизоваться в стиме (сохранение данных в файл необязательно);'
                   '\nexit  -  закрыть программу')
-
         else:
             print('Неверная команда (help  -  для доступа к списку комманд)')
 
--- steam_auth.py
+++ steam_auth.py
@@ -1,50 +1,73 @@
 from datetime import datetime
 import requests
+from Crypto.PublicKey import RSA
+from Crypto.Cipher import PKCS1_v1_5
+from base64 import b64encode
 
-requests.
 
 class SteamData:
-    def __init__(self, login, password):
-        if (type(login) and type(password) == str):
-            self.__login == login
-            self.__password == password
+    def __init__(self, login: str, password: str):
+        if type(login) == str and type(password) == str:
+            self.__login = login
+            self.__password = password
             self.__guard = ''
         else:
             raise Exception('Не валидные данные для входа в аккаунт')
 
     @property
-    def guard(self):
+    def guard(self) -> str:
         return self.__guard
 
     @guard.setter
-    def guard(self, guard):
-        if (type(guard) == str) and (len(list(guard)) == 5):
+    def guard(self, guard: str):
+        if (type(guard) == str) and (len(guard) == 5):
             self.__guard = guard
         else:
             raise Exception('Не валидный код 2FA')
 
     @property
-    def login(self):
+    def login(self) -> str:
         return self.__login
 
     @property
-    def password(self):
+    def password(self) -> str:
         return self.__password
 
-class SteamAuth:
-    def __init__(self, login, password, steam_guard):
-        if ((type(login) == str and type(password) == str and type(steam_guard) == str) and len(str(steam_guard)) == 5):
+class SteamClient:
+    def __init__(self, login: str, password: str, steam_guard: str):
+        if len(str(steam_guard)) == 5:
             self.__login = login
             self.__password = password
-            self.__guard = str(steam_guard).upper()
+            self.__guard = steam_guard.upper()
         else:
             raise Exception("Не валидные данные для входа в аккаунт")
-
-    def __get_timestamp_now(self):
+    @staticmethod
+    def __get_timestamp_now() -> int:
         return int(datetime.now().timestamp() * 1000)
 
-    def do(self):
-        self.__url = 'https://steamcommunity.com/'
+    @staticmethod
+    def __rsa_encrypt(message: str, publickey_mod: str, publickey_exp: str) -> bytes:
+        '''
+        :param message: шифруемое сообщение - str
+        :param publickey_mod: modulus открытого ключа - str - hexadecimal number
+        :param publickey_exp: exponent открытого ключа - str - hexadecimal number
+        :return: зашифрованное сообщение - bytes base64
+        '''
+        if type(message) != str:
+            raise Exception('RSA: Шифруемое сообщение должно являться строкой')
+        n = int(publickey_mod, 16)
+        e = int(publickey_exp, 16)
+        public_key = RSA.construct((n, e))
+        if not public_key.can_encrypt():
+            raise Exception('RSA: Неверный publickey для шифрования')
+        return b64encode(PKCS1_v1_5.new(public_key).encrypt(message.encode('ascii')))
+
+    def steam_auth(self):
+        '''
+        :
+        '''
+        self.__steam_url = 'https://steamcommunity.com/'
+
         headers = {
             "User-Agent":
                 "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Safari/537.36",
@@ -56,15 +79,44 @@ class SteamAuth:
             "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
             "X-Requested-With": "XMLHttpRequest",
             "Origin": "https://steamcommunity.com"
-            }
+        }
         with requests.session() as session:
-            response = session.
-            data = f'donotcache={self.__get_timestamp_now()}&username={self.__login}'
-            response = session.post(url=self.__url, data=data, headers=headers)
-            print(response.status_code)
-            print(response)
+            data = {
+                "donotcache": SteamClient.__get_timestamp_now(),
+                "username": self.__login
+            }
+            # 1-ый запрос
+            try:
+                response = session.post(url= self.__steam_url + 'login/getrsakey', data= data, headers= headers)
+                response_dict = response.json()
+            except Exception as ex:
+                raise Exception(f'Ошибка соединения: {ex}')
+            if response_dict['success'] == False:
+                raise Exception('Ошибка при авторизации в steam')
+            data = {
+                'username': self.__login,
+                "password": SteamClient.__rsa_encrypt(self.__password, response_dict['publickey_mod'],
+                                               response_dict['publickey_exp']),
+                "emailauth": '',
+                "emailsteamid": '',
+                "twofactorcode": self.__guard,
+                "captchagid": -1,
+                "captcha_text": '',
+                "loginfriendlyname": '',
+                "rsatimestamp": response_dict['timestamp'],
+                "remember_login": 'true',
+                "donotcache": SteamClient.__get_timestamp_now(),
+                "tokentype": -1
+            }
+            # 2-ой запрос
+            try:
+                response = session.post(url=self.__steam_url + 'login/dologin', data=data, headers=headers)
+                response_dict = response.json()
+            except Exception as ex:
+                raise Exception(f'Ошибка соединения: {ex}')
+            if response_dict['success'] == False:
+                raise Exception('Ошибка при авторизации в steam')
+
+            self.__session = session
 
 
-
-
-            data = f"donotcache={self.__get_timestamp_now()}'&password=rsa"
