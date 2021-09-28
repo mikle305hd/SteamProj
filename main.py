383efacb32949ebbac78695b05d52aa723869669
mikle305hd <mikle3305@gmail.com>
2021-09-29 02:15:21 +0300

    Случайный клик по дискард ченджес =(

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
-import steam_auth
 import json
+import os
+import pickle
+import steam_auth
 
 
 def main():
     user_data = None
+    cookies = None
     while True:
-        print('Введите комманду:', end= ' ')
+        print('Введите комманду:', end=' ')
         command = input().lower()
 
         if command == 'enter_userdata':
@@ -24,7 +27,7 @@ def main():
                 try:
                     with open('auth_data.json', 'w') as file:
                         data = {"username": user_data.login, "password": user_data.password}
-                        json.dump(data, file,indent=4)
+                        json.dump(data, file, indent=4)
                         del data
                     del user_data
                 except Exception as e:
@@ -42,8 +45,8 @@ def main():
                 with open('auth_data.json', 'r') as file:
                     data = json.load(file)
                     if data == {}:
-                        if user_data == None:
-                            print('Введите имя пользователя:', end= ' ')
+                        if user_data is None:
+                            print('Введите имя пользователя:', end=' ')
                             login = input()
                             print('Введите пароль:', end=' ')
                             password = input()
@@ -54,23 +57,68 @@ def main():
                     print('Введите код Steam Guard:', end=' ')
                     user_data.guard = input()
 
+                    print('Авторизация. Подождите...')
                     client = steam_auth.SteamClient(user_data.login, user_data.password, user_data.guard)
-                    client.steam_auth()
+                    cookies = client.steam_auth()
+                    print('Авторизация в steam прошла успешно.')
+                    print('Если хотите сохранить сессию в файл введите команду save:', end=' ')
+                    if input().lower() == 'save':
+                        try:
+                            with open('cookies.dat', 'wb') as pickle_file:
+                                pickle.dump(cookies, pickle_file)
+                        except Exception as e:
+                            print('Ошибка при сохранении сессии в файл', e)
+                            continue
             except Exception as e:
-                print(e)
+                print('Ошибка при авторизации в steam.\n', e)
+                continue
+
+        elif command == 'auth_csm':
+            print('Если хотите загрузить сессию из файла введите команду load:', end=' ')
+            if input().lower() == 'load':
+                if os.stat('cookies.dat') == 0:
+                    print('Ошибка - файл сессии пустой')
+                try:
+                    with open('cookies.dat', 'rb') as file:
+                        cookies = pickle.load(file)
+                except Exception as e:
+                    print('Ошибка при чтении файла сессии.', e)
+                    continue
+
+            if cookies is None:
+                print('Ошибка - авторизация steam не выполнена. Для дальнейших действий требуется auth_steam.')
+                continue
+            try:
+                print('Авторизация. Подождите...')
+                cookies = client.csm_auth(cookies)
+                print('Авторизация на old.cs.money прошла успешно.')
+                print('Если хотите сохранить сессию в файл введите команду save:', end=' ')
+                if input().lower() == 'save':
+                    try:
+                        with open('cookies.dat', 'wb') as file:
+                            pickle.dump(cookies, file)
+                    except Exception as e:
+                        print('Ошибка при сохранении сессии.', e)
+                        continue
+            except Exception as e:
+                print('Ошибка при авторизации на old.cs.money.\n', e)
+                continue
 
         elif command == 'exit':
-            print('Программа закрыта')
+            print('Программа закрыта.')
             return 0
 
         elif command == 'help':
             print('Список доступных комманд:'
-                  '\nenter_userdata  -  ввести/сохранить данные (имя пользователя и пароль) аккаунта в файл;'
-                  '\nclear_userdata  -  удалить данные аккаунта из файла'
-                  '\nauth_steam  -  авторизоваться в стиме (сохранение данных в файл необязательно);'
-                  '\nexit  -  закрыть программу')
+                  '\nenter_userdata  -  ввести/сохранить данные (имя пользователя и пароль) steam аккаунта в файл;'
+                  '\nclear_userdata  -  удалить данные steam аккаунта из файла'
+                  '\nauth_steam  -  авторизоваться в steam (сохранение данных в файл необязательно);'
+                  '\nauth_csm  -  авторизоваться на old.cs.money;'
+                  '\nexit  -  закрыть программу'
+                  )
         else:
             print('Неверная команда (help  -  для доступа к списку комманд)')
+
 
 if __name__ == '__main__':
     main()
--- steam_auth.py
+++ steam_auth.py
@@ -33,44 +33,49 @@ class SteamData:
     def password(self) -> str:
         return self.__password
 
+
+class Helpers:
+    @staticmethod
+    def get_timestamp_now() -> int:
+        return int(datetime.now().timestamp() * 1000)
+
+    @staticmethod
+    def rsa_encrypt(message: str, publickey_mod: int, publickey_exp: int) -> bytes:
+        """
+        RSA encrypt message with public key
+        :param message: шифруемое сообщение
+        :param publickey_mod: modulus открытого ключа
+        :param publickey_exp: exponent открытого ключа
+        :return: зашифрованное сообщение base64
+        """
+        if type(message) != str:
+            raise Exception('RSA: Шифруемое сообщение должно являться строкой')
+        public_key = RSA.construct((publickey_mod, publickey_exp))
+        if not public_key.can_encrypt():
+            raise Exception('RSA: Неверный publickey для шифрования')
+        return b64encode(PKCS1_v1_5.new(public_key).encrypt(message.encode('ascii')))
+
+
 class SteamClient:
     def __init__(self, login: str, password: str, steam_guard: str):
         if len(str(steam_guard)) == 5:
             self.__login = login
             self.__password = password
             self.__guard = steam_guard.upper()
+            self.__steam_url = 'https://steamcommunity.com/'
         else:
-            raise Exception("Не валидные данные для входа в аккаунт")
-    @staticmethod
-    def __get_timestamp_now() -> int:
-        return int(datetime.now().timestamp() * 1000)
-
-    @staticmethod
-    def __rsa_encrypt(message: str, publickey_mod: str, publickey_exp: str) -> bytes:
-        '''
-        :param message: шифруемое сообщение - str
-        :param publickey_mod: modulus открытого ключа - str - hexadecimal number
-        :param publickey_exp: exponent открытого ключа - str - hexadecimal number
-        :return: зашифрованное сообщение - bytes base64
-        '''
-        if type(message) != str:
-            raise Exception('RSA: Шифруемое сообщение должно являться строкой')
-        n = int(publickey_mod, 16)
-        e = int(publickey_exp, 16)
-        public_key = RSA.construct((n, e))
-        if not public_key.can_encrypt():
-            raise Exception('RSA: Неверный publickey для шифрования')
-        return b64encode(PKCS1_v1_5.new(public_key).encrypt(message.encode('ascii')))
+            raise Exception('Не валидные данные для входа в аккаунт')
 
     def steam_auth(self):
-        '''
-        :
-        '''
-        self.__steam_url = 'https://steamcommunity.com/'
+        """
+
+        :return:
+        """
 
         headers = {
             "User-Agent":
-                "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Safari/537.36",
+                "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
+                "Chrome/89.0.4389.90 Safari/537.36",
             "Host": "steamcommunity.com",
             "Accept-Language": "ru-Ru,ru;q=0.9",
             "sec-ch-ua":
@@ -82,31 +87,31 @@ class SteamClient:
         }
         with requests.session() as session:
             data = {
-                "donotcache": SteamClient.__get_timestamp_now(),
+                "donotcache": Helpers.get_timestamp_now(),
                 "username": self.__login
             }
             # 1-ый запрос
             try:
-                response = session.post(url= self.__steam_url + 'login/getrsakey', data= data, headers= headers)
+                response = session.post(url=self.__steam_url + 'login/getrsakey', data=data, headers=headers)
                 response_dict = response.json()
             except Exception as ex:
                 raise Exception(f'Ошибка соединения: {ex}')
-            if response_dict['success'] == False:
+            if not response_dict['success']:
                 raise Exception('Ошибка при авторизации в steam')
             data = {
                 'username': self.__login,
-                "password": SteamClient.__rsa_encrypt(self.__password, response_dict['publickey_mod'],
-                                               response_dict['publickey_exp']),
-                "emailauth": '',
-                "emailsteamid": '',
-                "twofactorcode": self.__guard,
-                "captchagid": -1,
-                "captcha_text": '',
-                "loginfriendlyname": '',
-                "rsatimestamp": response_dict['timestamp'],
-                "remember_login": 'true',
-                "donotcache": SteamClient.__get_timestamp_now(),
-                "tokentype": -1
+                'password': Helpers.rsa_encrypt(self.__password, int(response_dict['publickey_mod'], 16),
+                                                int(response_dict['publickey_exp'], 16)),
+                'emailauth': '',
+                'emailsteamid': '',
+                'twofactorcode': self.__guard,
+                'captchagid': -1,
+                'captcha_text': '',
+                'loginfriendlyname': '',
+                'rsatimestamp': response_dict['timestamp'],
+                'remember_login': 'true',
+                'donotcache': Helpers.get_timestamp_now(),
+                'tokentype': -1
             }
             # 2-ой запрос
             try:
@@ -114,9 +119,10 @@ class SteamClient:
                 response_dict = response.json()
             except Exception as ex:
                 raise Exception(f'Ошибка соединения: {ex}')
-            if response_dict['success'] == False:
+            if not response_dict['success']:
                 raise Exception('Ошибка при авторизации в steam')
 
-            self.__session = session
+            return session.cookies
 
-
+    def csm_auth(self, cookies):
+        return cookies
